fc2ecc840e0aaedc4a15bed368a316825943c3a4
Darrell-Devana <darrell.putra@binus.ac.id>
2024-10-30 12:03:13 +0700

    added dockerfile

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/Darrell-Devana/be-note-app/util"
+	"github.com/gin-gonic/gin"
 )
 
+func helloWorld(c *gin.Context) {
+	c.String(http.StatusOK, "Hello, world!")
+}
+
 func main() {
-	message := "Hello this is a message"
+	message := "be-note-app started"
 	messageUpper := util.ToUpperCase(message)
 	fmt.Println(messageUpper)
+
+	r := gin.Default()
+	r.GET("/hello", helloWorld)
+	r.Run()
 }
