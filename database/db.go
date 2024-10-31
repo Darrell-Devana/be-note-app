d28198a0d697038238534a3f8f4237a4ae802681
Darrell-Devana <darrell.putra@binus.ac.id>
2024-10-31 14:53:33 +0700

    added db dir

---
+++ database/db.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func ConnectDatabase() {
+	err := godotenv.Load("application")
+	if err != nil {
+		panic(err)
+	}
+
+	host := os.Getenv("HOST")
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	user := os.Getenv("USER")
+	dbname := os.Getenv("DB_NAME")
+	pass := os.Getenv("PASSWORD")
+
+	// set up postgres sql to open it.
+	psqlSetup := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+		host, port, user, dbname, pass)
+	db, errSql := sql.Open("postgres", psqlSetup)
+	if errSql != nil {
+		fmt.Println("There is an error while connecting to the database ", errSql)
+		panic(errSql)
+	} else {
+		DB = db
+		fmt.Println("Successfully connected to database!")
+	}
+}
--- main.go
+++ main.go
@@ -1,22 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/Darrell-Devana/be-note-app/util"
 	"github.com/gin-gonic/gin"
 )
 
 func helloWorld(c *gin.Context) {
-	c.String(http.StatusOK, "Hello, world!")
+	c.String(http.StatusOK, "Hello, world!\n")
 }
 
 func main() {
-	message := "be-note-app started"
-	messageUpper := util.ToUpperCase(message)
-	fmt.Println(messageUpper)
-
 	r := gin.Default()
 	r.GET("/hello", helloWorld)
 	r.Run()
