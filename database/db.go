778b47a33737613de77135a02a573bff1c10e69c
Darrell-Devana <darrell.putra@binus.ac.id>
2024-11-01 11:13:27 +0700

    a lot of changes

--- database/db.go
+++ database/db.go
@@ -6,18 +6,12 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func ConnectDatabase() {
-	err := godotenv.Load("application")
-	if err != nil {
-		panic(err)
-	}
-
 	host := os.Getenv("HOST")
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
 	user := os.Getenv("USER")
@@ -27,12 +21,15 @@ func ConnectDatabase() {
 	// set up postgres sql to open it.
 	psqlSetup := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
 		host, port, user, dbname, pass)
-	db, errSql := sql.Open("postgres", psqlSetup)
-	if errSql != nil {
-		fmt.Println("There is an error while connecting to the database ", errSql)
-		panic(errSql)
-	} else {
-		DB = db
-		fmt.Println("Successfully connected to database!")
+	db, err := sql.Open("postgres", psqlSetup)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
 	}
+	if errPing := db.Ping(); errPing != nil {
+		fmt.Println("Unable to connect to the database:", errPing)
+		panic(errPing)
+	}
+	DB = db
+	fmt.Println("Successfully connected to database!")
 }
--- main.go
+++ main.go
@@ -1,10 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Darrell-Devana/be-note-app/database"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 )
+
+type Note struct {
+	Title      string    `json:"title"`
+	Content    string    `json:"content"`
+	CreatedAt  string    `json:"createdAt"`
+	UpdatedAt  string    `json:"updatedAt"`
+	ID         uuid.UUID `json:"id"`
+	IsFavorite bool      `json:"isFavorite"`
+}
+
+func addNote(c *gin.Context) {
+	note := Note{}
+	if err := c.ShouldBindJSON(&note); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"code": "400", "message": "Title and content is required"})
+		return
+	}
+
+	if note.Content == "" || note.Title == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"code": "400", "message": "Title and content is required"})
+		return
+	}
+
+	fmt.Println("Title:", note.Title, "Content:", note.Content)
+
+	_, errSql := database.DB.Exec("insert into notes(content, title) values ($1, $2)", note.Content, note.Title)
+	if errSql != nil {
+		fmt.Println(errSql)
+		c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Database operation failed"})
+		return
+	}
+
+	c.String(http.StatusOK, note.Content)
+}
+
+func listNote(c *gin.Context) {
+	rows, err := database.DB.Query("select * from notes")
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Database operation failed"})
+		return
+	}
+
+	notes := []Note{}
+	for rows.Next() {
+		var note Note
+		err := rows.Scan(&note.ID, &note.Content, &note.Title, &note.IsFavorite, &note.CreatedAt, &note.UpdatedAt)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Failed to retrieve notes"})
+			return
+		}
+		notes = append(notes, note)
+	}
+	c.JSON(http.StatusOK, gin.H{"code": "200", "message": "List successful", "output": notes})
+}
 
 func helloWorld(c *gin.Context) {
 	c.String(http.StatusOK, "Hello, world!\n")
@@ -12,6 +71,16 @@ func helloWorld(c *gin.Context) {
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.Default())
+
+	err := godotenv.Load("application.env")
+	if err != nil {
+		panic(err)
+	}
+	database.ConnectDatabase()
 	r.GET("/hello", helloWorld)
-	r.Run()
+	r.POST("/add", addNote)
+	r.GET("/list", listNote)
+	r.Run(":8008")
 }
