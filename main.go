68a57f8f99ee50be655c7b1fa2f766fd62cf3e5b
Darrell-Devana <darrell.putra@binus.ac.id>
2024-11-11 15:04:23 +0700

    Potential final

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -12,36 +13,37 @@ import (
 )
 
 type Note struct {
-	Title      string    `json:"title"`
-	Content    string    `json:"content"`
-	CreatedAt  string    `json:"createdAt"`
-	UpdatedAt  string    `json:"updatedAt"`
-	ID         uuid.UUID `json:"id"`
-	IsFavorite bool      `json:"isFavorite"`
+	Title       string          `json:"title"`
+	CreatedAt   string          `json:"createdAt"`
+	UpdatedAt   string          `json:"updatedAt"`
+	LastVisited string          `json:"lastVisited"`
+	TextContent string          `json:"textContent"`
+	Content     json.RawMessage `json:"content"`
+	ID          uuid.UUID       `json:"id"`
+	IsFavorite  bool            `json:"isFavorite"`
 }
 
 func addNote(c *gin.Context) {
 	note := Note{}
 	if err := c.ShouldBindJSON(&note); err != nil {
+		fmt.Println(err)
 		c.JSON(http.StatusBadRequest, gin.H{"code": "400", "message": "Title and content is required"})
 		return
 	}
 
-	if note.Content == "" || note.Title == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"code": "400", "message": "Title and content is required"})
-		return
-	}
+	var noteID string
+	errSql := database.DB.QueryRow(
+		"INSERT INTO notes(content, title, is_favorite, text_content) VALUES ($1, $2, $3, $4) RETURNING id",
+		note.Content, note.Title, note.IsFavorite, note.TextContent,
+	).Scan(&noteID)
 
-	fmt.Println("Title:", note.Title, "Content:", note.Content)
-
-	_, errSql := database.DB.Exec("insert into notes(content, title, is_favorite) values ($1, $2, $3)", note.Content, note.Title, note.IsFavorite)
 	if errSql != nil {
 		fmt.Println(errSql)
 		c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Database operation failed"})
 		return
 	}
 
-	c.String(http.StatusOK, note.Content)
+	c.JSON(http.StatusOK, gin.H{"code": "200", "message": "Note created successfully", "id": noteID})
 }
 
 func listNote(c *gin.Context) {
@@ -55,8 +57,9 @@ func listNote(c *gin.Context) {
 	notes := []Note{}
 	for rows.Next() {
 		var note Note
-		err := rows.Scan(&note.ID, &note.Content, &note.Title, &note.IsFavorite, &note.CreatedAt, &note.UpdatedAt)
+		err := rows.Scan(&note.ID, (*[]byte)(&note.Content), &note.Title, &note.IsFavorite, &note.CreatedAt, &note.UpdatedAt, &note.LastVisited, &note.TextContent)
 		if err != nil {
+			fmt.Println(err)
 			c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Failed to retrieve notes"})
 			return
 		}
@@ -65,22 +68,125 @@ func listNote(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"code": "200", "message": "List successful", "output": notes})
 }
 
-func helloWorld(c *gin.Context) {
-	c.String(http.StatusOK, "Hello, world!\n")
+func openNote(c *gin.Context) {
+	id := c.Param("id")
+	fmt.Printf("%v\n", id)
+	rows, err := database.DB.Query("select * from notes where id = $1", id)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Database operation failed"})
+		return
+	}
+
+	notes := []Note{}
+	for rows.Next() {
+		var note Note
+		err := rows.Scan(&note.ID, (*[]byte)(&note.Content), &note.Title, &note.IsFavorite, &note.CreatedAt, &note.UpdatedAt, &note.LastVisited, &note.TextContent)
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Failed to retrieve notes"})
+			return
+		}
+		notes = append(notes, note)
+	}
+
+	_, err = database.DB.Exec("update notes set last_visited = now() where id = $1", notes[0].ID)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Database operation failed"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"code": "200", "message": "Note found successfully", "output": notes[0]})
+}
+
+func updateNote(c *gin.Context) {
+	note := Note{}
+	if err := c.ShouldBindJSON(&note); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"code": "400", "message": "Invalid request body. Please ensure all required fields are provided and correctly formatted."})
+		return
+	}
+
+	fmt.Println(note)
+
+	_, err := database.DB.Exec("update notes set content = $2, title = $3, is_favorite = $4, created_at = $5, updated_at = now(), last_visited = now(), text_content = $6 where id = $1", note.ID, note.Content, note.Title, note.IsFavorite, note.CreatedAt, note.TextContent)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Database operation failed"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"code": "200", "message": "Note updated successfully", "output": note})
+}
+
+func favoriteNote(c *gin.Context) {
+	id := c.Param("id")
+	fmt.Println(id)
+
+	rows, err := database.DB.Query("select * from notes where id = $1", id)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Database operation failed"})
+		return
+	}
+
+	notes := []Note{}
+	for rows.Next() {
+		var note Note
+		err := rows.Scan(&note.ID, (*[]byte)(&note.Content), &note.Title, &note.IsFavorite, &note.CreatedAt, &note.UpdatedAt, &note.LastVisited, &note.TextContent)
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Failed to retrieve notes"})
+			return
+		}
+		notes = append(notes, note)
+	}
+
+	if notes[0].IsFavorite {
+		_, err = database.DB.Exec("update notes set is_favorite = $2 where id = $1", id, false)
+	} else {
+		_, err = database.DB.Exec("update notes set is_favorite = $2 where id = $1", id, true)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Database operation failed"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"code": "200", "message": "Note deleted successfully", "deleted": id})
+}
+
+func deleteNote(c *gin.Context) {
+	id := c.Param("id")
+	fmt.Println(id)
+
+	_, err := database.DB.Exec("delete from notes where id = $1", id)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"code": "500", "message": "Database operation failed"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"code": "200", "message": "Note deleted successfully", "deleted": id})
 }
 
 func main() {
 	r := gin.Default()
-
 	r.Use(cors.Default())
 
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
+
 	database.ConnectDatabase()
-	r.GET("/hello", helloWorld)
+
 	r.POST("/add", addNote)
 	r.GET("/list", listNote)
+	r.GET("/notes/:id", openNote)
+	r.POST("/update", updateNote)
+	r.POST("/delete/:id", deleteNote)
+	r.POST("/favorite/:id", favoriteNote)
 	r.Run(":8008")
 }
